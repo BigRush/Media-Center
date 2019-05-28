b65638da0c407717815dd47563c5a5b562a276c2
BigRush <tom.harpaz98@gmail.com>
2019-05-28 12:41:57 +0300

    added setup.sh script

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+media_dir=Media-Server
+
+if ! [[ -f docker-compose.yml ]]; then
+    echo "Could not find 'docker-compose.yml'... setup.sh failed..."
+    exit 1
+fi
+
+if [[ -d $HOME/Media-Server ]]; then
+    echo "The directory '$HOME/Media-Server' already exists!"
+    echo "Please change its name or modify 'media_dir' variable in the script... exiting..."
+    exit 1
+fi
+    
+sed -i "s|<path to>|$HOME/$media_dir|g" docker-compose.yml 
+
+if [[ $? -ne 0 ]]; then
+    echo "Something went wrong with sed... exiting..."
+    exit 1
+fi
+
+for i in $(grep $HOME tmp |awk '{print $NF}' |cut -d ':' -f '1'); do
+    mkdir -p $i 
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to create directory '$i'... exiting..."
+        exit 1
+    fi
+done
+
+echo "Completed successfully. Persistent volumes have been created under $HOME/$media_dir. Have a nice day!"
+
